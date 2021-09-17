699e6cca35dfb3446af9050504bf77593a56caff
Misael Ruiz <misaruiz@gmail.com>
2021-09-16 21:31:43 -0700

    Home page iterates through the projects successfully

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { projects } = require('./data.json');
-const data = projects;
+// const { projects } = require('./data.json');
+const data = require('./data.json');
 
 const port = 3000;
 
@@ -14,15 +14,16 @@ app.set('view engine', 'pug');
 
 // Renders
 app.get('/', (req, res) => {
-    res.render('index', data.projects);
+    const projects = data.projects;
+    console.dir(projects);
+    res.render('index', {projects});
 });
 app.get('/about', (req, res) => {
     res.render('about', data.projects);
 })
 app.get('/project/:id', (req, res) => {
     const { id } = req.params;
-    console.dir(data[id].project_name);
-    const project = data[id];
+    const project = data.projects[id];
     res.render('project', project );
 })
 
