672d43b7924abbbec6c9eb595465dd6f7ddec077
Misael Ruiz <misaruiz@gmail.com>
2021-09-17 01:33:43 -0700

    Making styling updates

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
-// const { projects } = require('./data.json');
-const data = require('./data.json');
+const data = require('./data/data.json');
 
 const port = 3000;
 
@@ -11,22 +10,49 @@ app.use('/static', express.static('public'));
 
 app.set('view engine', 'pug');
 
-
 // Renders
 app.get('/', (req, res) => {
     const projects = data.projects;
-    console.dir(projects);
     res.render('index', {projects});
 });
 app.get('/about', (req, res) => {
-    res.render('about', data.projects);
+    res.render('about');
 })
-app.get('/project/:id', (req, res) => {
+app.get('/project/:id', (req, res, next) => {
     const { id } = req.params;
-    const project = data.projects[id];
-    res.render('project', project );
-})
+    const numOfProjects = Object.keys(data.projects).length;
 
+    if (typeof id === 'number' || id == parseInt(id)) {
+        if (parseInt(id) < numOfProjects) {
+            const project = data.projects[id];
+            res.render('project', project );
+        } else {
+            next();
+        }   
+    } else {
+        next();
+    }
+});
+
+// Handle 404 Errors
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// Handle other Errors
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    // Special page for 404 Errors
+    if (err.status === 404) {
+        res.render('page-not-found', err);
+    } else {
+        res.render('error');
+    }
+    
+});
 
 
 app.listen(port, () => {
